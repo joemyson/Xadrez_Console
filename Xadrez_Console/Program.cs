fd388ab8f68841e262404a0292c20cecb79fae87
joemyson-santos <joemyson1402@gmail.com>
2021-04-20 23:07:31 -0300

    iniciado o tabuleiro

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using tabuleiro;
 namespace Xadrez_Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
+
         }
     }
 }
