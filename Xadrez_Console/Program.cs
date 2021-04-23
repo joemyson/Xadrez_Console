c49e7e9d69c9feb1ce025825ee892ce53e9113d0
joemyson-santos <joemyson1402@gmail.com>
2021-04-23 00:34:14 -0300

    melhorando a inclusão das pecas

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -8,10 +8,23 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
-           
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.ColacarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
+                tab.ColacarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+                tab.ColacarPeca(new Rei(tab, Cor.Amarela), new Posicao(0, 2));
+
+                tab.ColacarPeca(new Torre(tab, Cor.Laranja), new Posicao(3, 5));
+                tab.ColacarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
             Console.ReadLine();
         }
     }
