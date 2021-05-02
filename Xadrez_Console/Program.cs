5023795d42be075fb53102a023159d2627ca19cb
joemyson-santos <joemyson1402@gmail.com>
2021-05-02 03:46:43 -0300

    teste inicias com movimentos possiveis das pecas

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -11,13 +11,22 @@ namespace Xadrez_Console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
+
                 while (!partida.terminada)
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    
                     Console.WriteLine();
                     Console.Write(" Origem");                   
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPosiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
                     Console.Write(" Destino ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
--- Xadrez_Console/tabuleiro/Posicao.cs
+++ Xadrez_Console/tabuleiro/Posicao.cs
@@ -9,7 +9,17 @@
         {
             this.linha = linha;
             this.coluna = coluna;
-        }       
+        } 
+        
+        public void definirValores(int linha,int coluna)
+        {
+            this.linha= linha;
+            this.coluna = coluna;
+
+
+        }
+
+
         public override string ToString()
         {
             return linha +","+coluna;
